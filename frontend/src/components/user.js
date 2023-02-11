d5d29f6f1f68bd44e8c4c0a28aa6ddddac0cc9d2
Raghav Rathi <raghav17083@iiitd.ac.in>
2023-02-11 19:32:16 +0530

    User component

--- frontend/src/components/user.js
+++ frontend/src/components/user.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useFetch } from "react-async";
 import { useEffect, useState } from "react";
-import { Button } from "antd";
+import { Button, List } from "antd";
+import _ from "lodash";
 const User = () => {
   // 1. Fetch all the products
+  const [list, showList] = useState(false);
   const [product, setProducts] = useState([]);
-  const listProduct = () => {
+  const fetchProducts = () => {
     axios.get("http://127.0.0.1:8080/products").then((response) => {
       const { data: products } = response.data;
       console.log(products);
@@ -13,12 +15,35 @@ const User = () => {
     });
   };
   useEffect(() => {
-    listProduct();
+    fetchProducts();
   }, []);
-  console.log({ product });
+
+  const listProducts = () => {
+    showList(!list);
+  };
+  const addToCart = ({itemId, quantity})=>{
+
+  }
+  const displayProduct = list && "Display products";
   return (
     <div>
-      <Button>List Products</Button>
+      <Button onClick={listProducts}>List Products</Button>
+      <div>
+        {list && (
+          <div>
+            <List
+              dataSource={product}
+              renderItem={(item) => (
+                <List.Item key={item.itemId}>
+                  <List.Item.Meta title={`item name: ${item.name}`} />
+                  price: {item.price}
+                  <Button onClick={}></Button>
+                </List.Item>
+              )}
+            ></List>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
